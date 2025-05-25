a34dfa3b5db8fe420bdd1ac1d6e3fe7d3e515ef7
bootyLicker <johnpastor53435@gmail.com>
2025-05-26 06:20:47 +0800

    Add project files.

---
+++ shopix/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace shopix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hopeForm2 = new ReaLTaiizor.Forms.HopeForm();
+            this.parrotNavigationBar1 = new ReaLTaiizor.Controls.ParrotNavigationBar();
+            this.parrotNavigationBar2 = new ReaLTaiizor.Controls.ParrotNavigationBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // hopeForm2
+            // 
+            this.hopeForm2.ControlBoxColorH = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(231)))), ((int)(((byte)(237)))));
+            this.hopeForm2.ControlBoxColorHC = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.hopeForm2.ControlBoxColorN = System.Drawing.Color.White;
+            this.hopeForm2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.hopeForm2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.hopeForm2.Font = new System.Drawing.Font("Sans Serif Collection", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hopeForm2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(246)))), ((int)(((byte)(252)))));
+            this.hopeForm2.Image = global::shopix.Properties.Resources.burnice;
+            this.hopeForm2.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.hopeForm2.Location = new System.Drawing.Point(0, 0);
+            this.hopeForm2.Name = "hopeForm2";
+            this.hopeForm2.Size = new System.Drawing.Size(1386, 40);
+            this.hopeForm2.TabIndex = 4;
+            this.hopeForm2.Text = "Shoppix";
+            this.hopeForm2.ThemeColor = System.Drawing.Color.DarkOrange;
+            // 
+            // parrotNavigationBar1
+            // 
+            this.parrotNavigationBar1.BackColor = System.Drawing.Color.Black;
+            this.parrotNavigationBar1.BackgroundColor = System.Drawing.Color.White;
+            this.parrotNavigationBar1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.parrotNavigationBar1.CompositingQualityType = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+            this.parrotNavigationBar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.parrotNavigationBar1.ForeColor = System.Drawing.Color.White;
+            this.parrotNavigationBar1.Interaction = true;
+            this.parrotNavigationBar1.InterpolationType = System.Drawing.Drawing2D.InterpolationMode.HighQualityBilinear;
+            this.parrotNavigationBar1.ItemColor = System.Drawing.Color.Gray;
+            this.parrotNavigationBar1.LeftCustomImage = null;
+            this.parrotNavigationBar1.LeftCustomText = "⫷⩶";
+            this.parrotNavigationBar1.LeftItem = ReaLTaiizor.Controls.ParrotNavigationBar.NavigationItem.Menu;
+            this.parrotNavigationBar1.Location = new System.Drawing.Point(0, 0);
+            this.parrotNavigationBar1.Name = "parrotNavigationBar1";
+            this.parrotNavigationBar1.NavBarStyle = ReaLTaiizor.Controls.ParrotNavigationBar.Style.Android;
+            this.parrotNavigationBar1.RightCustomImage = null;
+            this.parrotNavigationBar1.RightCustomText = "⩶⫸";
+            this.parrotNavigationBar1.RightItem = ReaLTaiizor.Controls.ParrotNavigationBar.NavigationItem.None;
+            this.parrotNavigationBar1.Size = new System.Drawing.Size(226, 50);
+            this.parrotNavigationBar1.TabIndex = 6;
+            this.parrotNavigationBar1.Text = "parrotNavigationBar1";
+            this.parrotNavigationBar1.TextRenderingType = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            this.parrotNavigationBar1.Title = "Home";
+            this.parrotNavigationBar1.TitleColor = System.Drawing.Color.Gray;
+            this.parrotNavigationBar1.MouseEnter += new System.EventHandler(this.parrotNavigationBar1_MouseEnter);
+            // 
+            // parrotNavigationBar2
+            // 
+            this.parrotNavigationBar2.BackColor = System.Drawing.Color.Black;
+            this.parrotNavigationBar2.BackgroundColor = System.Drawing.Color.White;
+            this.parrotNavigationBar2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.parrotNavigationBar2.CompositingQualityType = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+            this.parrotNavigationBar2.ForeColor = System.Drawing.Color.White;
+            this.parrotNavigationBar2.Interaction = true;
+            this.parrotNavigationBar2.InterpolationType = System.Drawing.Drawing2D.InterpolationMode.HighQualityBilinear;
+            this.parrotNavigationBar2.ItemColor = System.Drawing.Color.Gray;
+            this.parrotNavigationBar2.LeftCustomImage = null;
+            this.parrotNavigationBar2.LeftCustomText = "⫷⩶";
+            this.parrotNavigationBar2.LeftItem = ReaLTaiizor.Controls.ParrotNavigationBar.NavigationItem.Menu;
+            this.parrotNavigationBar2.Location = new System.Drawing.Point(0, 56);
+            this.parrotNavigationBar2.Name = "parrotNavigationBar2";
+            this.parrotNavigationBar2.NavBarStyle = ReaLTaiizor.Controls.ParrotNavigationBar.Style.Android;
+            this.parrotNavigationBar2.RightCustomImage = null;
+            this.parrotNavigationBar2.RightCustomText = "⩶⫸";
+            this.parrotNavigationBar2.RightItem = ReaLTaiizor.Controls.ParrotNavigationBar.NavigationItem.None;
+            this.parrotNavigationBar2.Size = new System.Drawing.Size(226, 50);
+            this.parrotNavigationBar2.TabIndex = 7;
+            this.parrotNavigationBar2.Text = "parrotNavigationBar2";
+            this.parrotNavigationBar2.TextRenderingType = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            this.parrotNavigationBar2.Title = "Items";
+            this.parrotNavigationBar2.TitleColor = System.Drawing.Color.Gray;
+            this.parrotNavigationBar2.MouseEnter += new System.EventHandler(this.mouse_enter);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.parrotNavigationBar1);
+            this.panel1.Controls.Add(this.parrotNavigationBar2);
+            this.panel1.Location = new System.Drawing.Point(12, 58);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(226, 735);
+            this.panel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(279, 75);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(226, 50);
+            this.panel2.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.pictureBox1.InitialImage = global::shopix.Properties.Resources.camellya;
+            this.pictureBox1.Location = new System.Drawing.Point(21, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(31, 32);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1386, 805);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.hopeForm2);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximumSize = new System.Drawing.Size(1920, 1020);
+            this.MinimumSize = new System.Drawing.Size(190, 40);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "formTheme1";
+            this.TransparencyKey = System.Drawing.Color.Purple;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private ReaLTaiizor.Forms.HopeForm hopeForm2;
+        private ReaLTaiizor.Controls.ParrotNavigationBar parrotNavigationBar1;
+        private ReaLTaiizor.Controls.ParrotNavigationBar parrotNavigationBar2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ shopix/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace shopix
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mouse_enter (object sender, EventArgs e)
+        {
+            this.ForeColor = Color.LightGray;
+        }
+
+        private void mouse_click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.DarkOrange;
+        }
+
+        private void parrotNavigationBar1_MouseHover(object sender, EventArgs e)
+        {
+            this.ForeColor = Color.LightGray;
+        }
+
+        private void parrotNavigationBar1_MouseEnter(object sender, EventArgs e)
+        {
+            this.ForeColor = Color.DarkOrange;
+        }
+    }
+}
